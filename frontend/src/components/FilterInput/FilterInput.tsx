3d02ee7e20c48075df898d8176c4d3a65b2d2b71
Filype Abreu <62102835+zfrekey@users.noreply.github.com>
2025-11-26 18:45:18 -0300

    feat: input de pesquisa de usuários

--- frontend/src/components/FilterInput/FilterInput.tsx
+++ frontend/src/components/FilterInput/FilterInput.tsx
@@ -1,1 +1,31 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import Image from 'next/image';
+
+interface FilterInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function FilterInput({  
+  value, 
+  onChange 
+}: FilterInputProps) {
+  return (
+    <div className={styles.inputWrapper}>
+      <Image 
+        src="/search.png"
+        alt="Search icon"
+        width={20}
+        height={20}
+        className={styles.searchIcon}
+      />
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Buscar por Nome ou CPF..."
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
